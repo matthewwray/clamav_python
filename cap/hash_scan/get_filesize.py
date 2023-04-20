8f349ad593e6ea18bb4e7e4c0a06a0818dc692c0
Matthew Wray <54137850+matthewwray@users.noreply.github.com>
2023-04-20 22:29:44 +0100

    Delete cap directory

--- cap/hash_scan/get_filesize.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def get_filesize(filename):
-    filesize = os.path.getsize(filename)
-    return filesize
--- cap/sigtool/OLD/test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int i;
-    for (i=0; i<256; i++){
-        printf("%x,%c,\n", i, i);
-    }
-}
